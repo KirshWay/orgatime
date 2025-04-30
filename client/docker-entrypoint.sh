f71622e0963750248102210c188d42f601c8c603
Viktor Nemets <nemecvictor.kirsh@gmail.com>
2025-04-30 15:10:57 +0300

    fix: configs, deleted comments

--- client/docker-entrypoint.sh
+++ client/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# Замена переменных окружения в конфигурации nginx
 envsubst '${VITE_BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Запускаем nginx
 exec "$@"
